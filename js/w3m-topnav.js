20870f03777f7d8e2cbc8b2ffff984f2a589bf29
Alwyn Barry <alwyn_barry@yahoo.co.uk>
2025-10-06 13:33:19 +0100

    Minor change to add license to js file
    Minor change to add license to js file

--- js/w3m-topnav.js
+++ js/w3m-topnav.js
@@ -1,7 +1,21 @@
 /**
  * Name: w3m-topnav
+ * 
  * Descripton: Scripts to create a w3.css based horizontal menu that re-orients to a sidebar vertical menu on mobiles.
- * @author: Alwyn Barry
+ * 
+ * @author		Alwyn Barry
+ * @copyright	2021
+ * For use in	w3-theme
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @version		1.0.0
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the License, 
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
  *
  * Guide: To work correctly the menu should be structured as follows:
  * 
@@ -22,6 +36,7 @@
  *					<!-- The dropdown content part -->
  *					<div class="w3-dropdown-content w3-bar-block w3-card-4">
  *						<a class="w3-bar-item w3-button">link 1</a>
+ *						 <span class="w3-bar-item">Text non-link</span>
  *						<a class="w3-bar-item w3-button">link 2</a>
  * 						<!-- ... additional links, including additional dropdowns ... -->
  *					</div> <!-- w3-dropdown-content -->
